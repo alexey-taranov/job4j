b7b41dca8434a136fa63e8b133b433f4b2e48aff
alexey-taranov <penspinner97@yandex.ru>
2019-03-23 23:55:32 +0300

    add JavaDoc

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,19 +1,42 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-
+    /**
+     * Сложение.
+     * @param first первое число.
+     * @param second второе число.
+     * @return результат.
+     */
     public double add(double first, double second) {
         return first + second;
     }
 
+    /**
+     * Вычитание.
+     * @param first первое число.
+     * @param second второе число.
+     * @return результат.
+     */
     public double substract(double first, double second) {
         return first - second;
     }
 
+    /**
+     * Деление.
+     * @param first первое число.
+     * @param second второе число.
+     * @return результат.
+     */
     public double div(double first, double second) {
         return first / second;
     }
 
+    /**
+     * Умножение.
+     * @param first первое число.
+     * @param second второе число.
+     * @return результат.
+     */
     public double multiple(double first, double second) {
         return first * second;
     }
