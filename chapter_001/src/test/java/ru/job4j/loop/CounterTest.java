b0fa02a3a59d17ef298a788c828516ec2e036b8c
alexey-taranov <penspinner97@yandex.ru>
2019-03-24 22:03:06 +0300

    rename method

--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -8,9 +8,9 @@ import static org.junit.Assert.assertThat;
 public class CounterTest {
 
     @Test
-    public void whenSecondMax() {
+    public void when1to10() {
        Counter counter = new Counter();
-        int result = counter.add(0, 10);
-        assertThat(result, is(30));
+       int result = counter.add(0, 10);
+       assertThat(result, is(30));
     }
 }
