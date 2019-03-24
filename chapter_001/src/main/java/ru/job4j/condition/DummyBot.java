8a9e38b65e1787c94024e72d96229c2d384933ec
alexey-taranov <penspinner97@yandex.ru>
2019-03-24 18:28:23 +0300

    add DummyBot

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author Alexey Taranov
+ * @version 1.0
+ * @since 24.03.2019
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
