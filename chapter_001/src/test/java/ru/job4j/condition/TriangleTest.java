99372631776ca5408f1d3a6e0dc1e3b5c8299672
alexey-taranov <penspinner97@yandex.ru>
2019-03-24 20:26:32 +0300

    add Triangle

---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+
+public class TriangleTest {
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Triangle triangle = new Triangle();
+        double result = triangle.area(0, 0, 0, 2, 2, 0);
+        double expected = 2D;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
