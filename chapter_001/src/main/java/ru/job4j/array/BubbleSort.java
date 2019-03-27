5eef643e14066d22a8291122cde31cc2354f49cc
alexey-taranov <penspinner97@yandex.ru>
2019-03-27 19:33:30 +0300

    refactor BubbleSort

--- chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
+++ chapter_001/src/main/java/ru/job4j/array/BubbleSort.java
@@ -3,11 +3,9 @@ package ru.job4j.array;
 public class BubbleSort {
 
     public int[] sort(int[] array) {
-
-        for (int i = 1; i != array.length; i++) {
-            for (int index = 1; index != array.length; index++) {
-
-                if (index + 1 != array.length && array[index] > array[index + 1]) {
+        for (int i = array.length - 1; i != 0; i--) {
+            for (int index = 0; index != i; index++) {
+                if (array[index] > array[index + 1]) {
                     int temp = array[index];
                     array[index] = array[index + 1];
                     array[index + 1] = temp;
