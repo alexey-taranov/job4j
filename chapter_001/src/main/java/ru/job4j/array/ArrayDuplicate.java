4d3ea6817bc1dd03cf7b9ab920cfe474476b6041
alexey-taranov <penspinner97@yandex.ru>
2019-03-28 20:25:06 +0300

    add ArrayDuplicate

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class ArrayDuplicate {
+    /**
+     * проверяет массив на наличие повторных строк.
+     * @param array массив.
+     * @return массив без повторов.
+     */
+
+    public String[] remove(String[] array) {
+        int unique = array.length;
+        for (int out = 0; out < unique; out++) {
+            for (int in = out + 1; in < unique; in++) {
+                if (array[out].equals(array[in])) {
+                    array[in] = array[unique - 1];
+                    unique--;
+                    in--;
+                }
+            }
+        }
+        return Arrays.copyOf(array, unique);
+    }
+}
