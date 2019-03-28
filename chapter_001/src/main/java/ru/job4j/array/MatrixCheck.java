8caa556aa8196a4ffa3a2c75d51ce9e7f825d3c8
alexey-taranov <penspinner97@yandex.ru>
2019-03-28 18:56:17 +0300

    add MatrixCheck

---
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    /**
+     * проверяет, что все элементы по диагоналям равны true или false.
+     * @param data массив.
+     * @return результат.
+     */
+    public boolean mono(boolean[][] data) {
+        boolean result = true;
+        for(int i = 0; i != data.length - 1 ; i++) {
+            if (data[i][i] != data[i + 1][i + 1] ||
+                    data[i][data.length - 1 - i] != data[i + 1][data.length - 2 - i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
