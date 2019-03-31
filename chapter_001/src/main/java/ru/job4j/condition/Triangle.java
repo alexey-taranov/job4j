3cd455a8757b4d9932516851e2e127809e132a12
alexey-taranov <penspinner97@yandex.ru>
2019-03-31 20:55:24 +0300

    refactor Triangle

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,6 +1,15 @@
 package ru.job4j.condition;
 
 public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point ap, Point bp, Point cp) {
+        this.first = ap;
+        this.second = bp;
+        this.third = cp;
+    }
 
     /**
      * Метод вычисления периметра по длинам сторон.
@@ -12,29 +21,29 @@ public class Triangle {
      * @param a расстояние между точками a b
      * @param b расстояние между точками a c
      * @param c расстояние между точками b c
-     * @return Перимент.
+     * @return Периметр.
      */
     public double period(double a, double b, double c) {
         return (a + b + c) / 2;
     }
 
     /**
-     * Метод должен вычислить прощадь треугольканива.
+     * Метод должен вычислить площадь треугольника.
      *
      * Формула.
      *
-     * √ p *(p - a) * (p - b) * (p - c)
+     * √ p *(p - a) * (p - a) * (p - c)
      *
      * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
      *
-     * @return Вернуть прощадь, если треугольник существует или -1.
+     * @return Вернуть площадь, если треугольник существует или -1.
      */
-    public double area(int x1, int y1, int x2, int y2, int x3, int y3) {
+    public double area() {
         double rsl = -1;
-        double a = new Point().distance(x1, y1, x2, y2);
-        double b = new Point().distance(x2, y2, x3, y3);
-        double c = new Point().distance(x1, y1, x3, y3);
-        double p = this.period(a, b, c);
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
         if (this.exist(a, b, c)) {
             return Math.sqrt(p * (p - a) * (p - b) * (p - c));
         }
@@ -51,7 +60,7 @@ public class Triangle {
      * @param c Длина от точки b c.
      * @return
      */
-    private boolean exist(double a, double c, double b) {
+    private boolean exist(double a, double b, double c) {
         return (a < b + c) && (b < a + c) && (c < b + a);
     }
 }
