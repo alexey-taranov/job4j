9fc8416322a830697063b2ebf40667bf12963e8d
alexey-taranov <penspinner97@yandex.ru>
2019-04-05 23:11:35 +0300

    refactor Item

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class Item {
     private String id;
     private String name;
-    private String decs;
+    private String desc;
     private long time;
 
-    public Item(String name, String decs, long time) {
+    public Item(String name, String desc, long time) {
         this.name = name;
-        this.decs = decs;
+        this.desc = desc;
         this.time = time;
     }
 
@@ -30,12 +30,12 @@ public class Item {
         this.name = name;
     }
 
-    public String getDecs() {
-        return decs;
+    public String getDesc() {
+        return desc;
     }
 
-    public void setDecs(String decs) {
-        this.decs = decs;
+    public void setDesc(String desc) {
+        this.desc = desc;
     }
 
     public long getTime() {
@@ -54,11 +54,11 @@ public class Item {
         return time == item.time &&
                 Objects.equals(id, item.id) &&
                 Objects.equals(name, item.name) &&
-                Objects.equals(decs, item.decs);
+                Objects.equals(desc, item.desc);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, decs, time);
+        return Objects.hash(id, name, desc, time);
     }
 }
