8bfab99effdf397303d8b4e5c1beb3e23f5e02b5
alexey-taranov <penspinner97@yandex.ru>
2019-04-07 21:01:48 +0300

    add pseudo

---
+++ chapter_002/src/main/java/ru/job4j/pseudo/Main.java
@@ -0,0 +1,9 @@
+package ru.job4j.pseudo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Paint().draw(new Triangle());
+        new Paint().draw(new Square());
+    }
+}
