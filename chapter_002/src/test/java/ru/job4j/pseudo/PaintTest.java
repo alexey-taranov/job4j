09e2d84ec579b1f7eca89a363c9f4b76533d3bfc
alexey-taranov <penspinner97@yandex.ru>
2019-04-08 22:29:25 +0300

    fix PaintTest

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -66,57 +66,5 @@ public class PaintTest {
                 )
         );
     }
-
 }
-//    @Test
-//    public void whenDrawSquare() {
-//        this.loadOutput();
-//        new Paint().draw(new Square());
-//        assertThat(
-//                this.out.toString(),
-//                is(
-//                        new StringBuilder()
-//                                .append("++++")
-//                                .append(System.lineSeparator())
-//                                .append("+  +")
-//                                .append(System.lineSeparator())
-//                                .append("+  +")
-//                                .append(System.lineSeparator())
-//                                .append("++++")
-//                                .append(System.lineSeparator())
-//                                .toString()
-//                )
-//        );
-//        System.setOut(stdout);
-//    }
-//
-//    @Test
-//    public void whenDrawTriangle() {
-//        // получаем ссылку на стандартный вывод в консоль.
-//        PrintStream stdout = System.out;
-//        // Создаем буфер для хранения вывода.
-//        ByteArrayOutputStream out = new ByteArrayOutputStream();
-//        //Заменяем стандартный вывод на вывод в пямять для тестирования.
-//        System.setOut(new PrintStream(out));
-//        // выполняем действия пишущие в консоль.
-//        new Paint().draw(new Triangle());
-//        // проверяем результат вычисления
-//        assertThat(
-//                new String(out.toByteArray()),
-//                is(
-//                        new StringBuilder()
-//                                .append("   +   ")
-//                                .append(System.lineSeparator())
-//                                .append("  + +  ")
-//                                .append(System.lineSeparator())
-//                                .append(" +   + ")
-//                                .append(System.lineSeparator())
-//                                .append("+++++++")
-//                                .append(System.lineSeparator())
-//                                .toString()
-//                )
-//        );
-//        // возвращаем обратно стандартный вывод в консоль.
-//        System.setOut(stdout);
-//    }
-//}
+
