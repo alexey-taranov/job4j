3d8321d55b905288a9a8be99385ec688dd970777
alexey-taranov <penspinner97@yandex.ru>
2019-04-13 20:03:58 +0300

    add Priority Queue

---
+++ chapter_002/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import java.util.LinkedList;
+
+public class PriorityQueue {
+    private LinkedList<Task> tasks = new LinkedList<>();
+
+    /**
+     * Метод должен вставлять в нужную позицию элемент.
+     * Позиция определять по полю приоритет.
+     * Для вставик использовать add(int index, E value)
+     * @param task задача
+     */
+    public void put(Task task) {
+        int index = 0;
+        for (int i = 0; i < tasks.size(); i++) {
+            index = i;
+            if (tasks.get(i).getPriority() > task.getPriority()) {
+                break;
+            }
+        }
+        tasks.add(index, task);
+    }
+
+    public Task take() {
+        return this.tasks.poll();
+    }
+}
+
