dd74e9df6fca4d877cea9a36b4f380bc759e7ed1
alexey-taranov <penspinner97@yandex.ru>
2019-09-02 19:20:51 +0300

    Создать элементарную структуру дерева

---
+++ chapter_005/src/main/java/ru/job4j/tree/Tree.java
@@ -0,0 +1,65 @@
+package ru.job4j.tree;
+
+import java.util.*;
+
+public class Tree<E extends Comparable<E>> implements SimpleTree<E> {
+
+    private final Node<E> root;
+
+    public Tree(E root) {
+        this.root = new Node<>(root);
+    }
+
+    @Override
+    public boolean add(E parent, E child) {
+        boolean rst = false;
+        Optional<Node<E>> parentNode = findBy(parent);
+        if (parentNode.isPresent() && findBy(child).isEmpty()) {
+            parentNode.get().add(new Node<>(child));
+            rst = true;
+        }
+        return rst;
+    }
+
+    @Override
+    public Optional<Node<E>> findBy(E value) {
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(this.root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.eqValue(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            for (Node<E> child : el.leaves()) {
+                data.offer(child);
+            }
+        }
+        return rsl;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        Queue<Node<E>> listIterator = new LinkedList<>();
+        listIterator.offer(this.root);
+        return new Iterator<>() {
+            @Override
+            public boolean hasNext() {
+                return !listIterator.isEmpty();
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Node<E> result = listIterator.poll();
+                for (Node<E> child : result.leaves()) {
+                    listIterator.offer(child);
+                }
+                return result.getValue();
+            }
+        };
+    }
+}
